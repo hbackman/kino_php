76dc5aafd50cc1a614b1abc5fc5eb18f1f6916aa
Hampus Backman <hampus@hbackman.com>
2024-05-29 21:22:41 -0400

    Updated mix.exs after reading hex.pm publishing guide.

--- mix.exs
+++ mix.exs
@@ -24,17 +24,14 @@ defmodule KinoPHP.MixProject do
   defp deps do
     [
       {:kino, "~> 0.12.2"}
-      # {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
   defp package do
     [
       maintainers: ["Hampus Backman"],
-      licenses: ["Apache-2.0"],
-      links: %{}
+      licenses: ["MIT"],
+      links: %{"GitHub": "https://github.com/hbackman/kino_php"}
     ]
   end
 end
